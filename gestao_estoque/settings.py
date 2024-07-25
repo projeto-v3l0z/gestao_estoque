d716e927787de51922e4a3e1cf3e1fff28626961
Thiago Menezes <thimenezes19@gmail.com>
2024-07-25 11:37:47 -0300

    fix: remove lib

--- gestao_estoque/settings.py
+++ gestao_estoque/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-import matplotlib
-matplotlib.use('Agg')
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
