0ae44a7746abbaec5616d7a394b528f06a7e1ab3
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2025-03-17 20:23:20 -0300

    fix: header

--- inventory_management/models.py
+++ inventory_management/models.py
@@ -71,6 +71,8 @@ class Product(models.Model):
     image = models.ImageField("Imagem", upload_to='product_images', blank=True, null=True)
     ncm = models.CharField("NCM", max_length=8, null=True, blank=True)
     slug = models.SlugField("Slug", max_length=100, blank=True, null=True, editable=False)
+    code1 = models.CharField("Código 1", max_length=100, blank=True, null=True)
+    code2 = models.CharField("Código 2", max_length=100, blank=True, null=True)
     color = models.ForeignKey('inventory_management.Color', on_delete=models.CASCADE, verbose_name="Cor", blank=True, null=True, editable=False)
     pattern = models.ForeignKey('inventory_management.Pattern', on_delete=models.CASCADE, verbose_name="Estampa", blank=True, null=True, editable=False)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('Criado por'), on_delete=models.CASCADE, related_name='product_created_by', null=True, editable=False)
--- inventory_management/views.py
+++ inventory_management/views.py
@@ -479,7 +479,9 @@ def generate_qr_codes(request):
                     c.drawInlineImage(qr, x_coordinate, y_coordinate, width=qr_size, height=qr_size)
 
                     # Código do produto (abaixo do QR Code)
-                    product_code = item.code
+                    product_code1 = item.code1
+                    product_code2 = item.code2
+                    product_code = f"Código 1: {product_code1} - Código 2: {product_code2}"
                     text_width = c.stringWidth(product_code, "VeraBd", 9)
                     code_x = x_coordinate + (qr_size - text_width) / 2
                     c.drawString(code_x, y_coordinate - 15, product_code)
